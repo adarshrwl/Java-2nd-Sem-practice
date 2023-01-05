6b7b63a72c507290b49a7680be0867934e9babb0
adarshrwl <adarshrawal321@gmail.com>
2023-01-05 22:38:41 +0545

    5th question

---
+++ 3.2/E.java
@@ -0,0 +1,11 @@
+//5. Write a JAVA program to check whether a number is even or odd.
+public class E {
+    public static void main(String[] args) {
+        int num1 = 5;
+        if (num1 % 2 == 0) {
+            System.out.println("Ït is even number");
+        } else {
+            System.out.println("IT is odd number");
+        }
+    }
+}
