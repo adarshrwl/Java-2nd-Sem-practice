7aeb95f6e0b9df1874ca85142167ece321f2ced7
adarshrwl <adarshrawal321@gmail.com>
2023-01-05 22:36:05 +0545

    4th question

--- 3.2/D.java
+++ 3.2/D.java
@@ -1,6 +1,11 @@
-
+//4. Write a JAVA program to check whether a number is divisible by 5 and 11 or not.
 public class D {
     public static void main(String[] args) {
-
+        int num1 = 10;
+        if (num1 % 5 == 0 && num1 % 11 == 0) {
+            System.out.println("It is divisible by 5 and 11");
+        } else {
+            System.out.println("It is not divisible by 5 and 11");
+        }
     }
 }
