c81fc597f700b2686b4c60413d68af8bf7c7b50b
adarshrwl <adarshrawal321@gmail.com>
2023-01-05 23:02:08 +0545

    9 Number Question

--- 3.2/I.java
+++ 3.2/I.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 //9. Write a JAVA program to input any character and check whether it is alphabet, digit or special
 //character.
 public class I {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your character alphabet or digit");
+        char ch = sc.next().charAt(0);
+        if (Character.isAlphabetic(ch)) {
+            System.out.println("It is alphabet");
+        } else if (Character.isDigit(ch)) {
+            System.out.println("It is Digit");
+        } else {
+            System.out.println("It is Special CHaracter");
+        }
+    }
 
 }
