768a2ff55ff5d0ae7a98500738c4a8b037373cd5
adarshrwl <adarshrawal321@gmail.com>
2023-01-06 12:08:42 +0545

    Looping basics

---
+++ For_Loop/Nested_Loop.java
@@ -0,0 +1,10 @@
+public class Nested_Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("VAlue of i is :" + i);
+            for (int j = 1; j < 10; j++) {
+                System.out.println("Apple");
+            }
+        }
+    }
+}
