ee1297846cf34d4d55d18e1a7ffd9e14135ecbfb
adarshrwl <adarshrawal321@gmail.com>
2023-01-06 09:57:45 +0545

    Ternay Operations

---
+++ Ternary Operator/A.java
@@ -0,0 +1,7 @@
+public class A {
+    public static void main(String[] args) {
+        int a = 20;
+        String value = (a >= 70) ? "It is greater than 70" : "It is Smaller than 70";
+        System.out.println(value);
+    }
+}
