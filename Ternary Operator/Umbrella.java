37e67df839bd3f417288087ed7d6bfde6e1b8d9b
adarshrwl <adarshrawal321@gmail.com>
2023-01-06 10:04:21 +0545

    Umbrella problem

---
+++ Ternary Operator/Umbrella.java
@@ -0,0 +1,8 @@
+
+public class Umbrella {
+    public static void main(String[] args) {
+        boolean isRaning = true;
+        String Value = (isRaning == true) ? "Take Umbrella while going outside" : "Dont take umbrella";
+        System.out.println(Value);
+    }
+}
