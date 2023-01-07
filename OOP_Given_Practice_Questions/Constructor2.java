66f7392d753ec71a3169c749d6350943df0ce4c6
adarshrwl <adarshrawal321@gmail.com>
2023-01-08 00:13:47 +0545

    Constructor

---
+++ OOP_Given_Practice_Questions/Constructor2.java
@@ -0,0 +1,17 @@
+/*There are 2 types of constructor
+ * 1)Non Paramitarized Constructor/Default Constructor
+ * 2)Paramitarized Constructor
+ */
+
+public class Constructor2 {
+    int a;
+    Float b;
+
+    public static void main(String[] args) {
+        Constructor2 c = new Constructor2();
+        System.out.println("Default Value");
+        System.out.println("a=" + c.a);
+        System.out.println("b=" + c.b);
+
+    }
+}
