9530cd0529810b040df9a391374b146a0f0f2d74
adarshrwl <adarshrawal321@gmail.com>
2023-01-08 00:44:34 +0545

    Arrays Questions Solutions

---
+++ Arrays/Arr3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//Storing USing Loop
+public class Arr3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] array1 = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Enter your value for position " + i);
+            int val = sc.nextInt();
+            array1[i] = val;
+        }
+
+        System.out.println("Your input values are :");
+        for (int j = 0; j < 10; j++) {
+            System.out.println(array1[j]);
+        }
+    }
+}
