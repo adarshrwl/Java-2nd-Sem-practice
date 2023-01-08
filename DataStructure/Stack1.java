df670271482e2d0a23efdbd472f3512bb29eec26
adarshrwl <adarshrawal321@gmail.com>
2023-01-08 17:36:29 +0545

    Stack

--- DataStructure/Stack1.java
+++ DataStructure/Stack1.java
@@ -9,7 +9,7 @@ public class Stack1 {
         this.top = -1;
     }
 
-    public Boolean isEmpty(int top) {
+    public Boolean isEmpty() {
         if (top == -1) {
             return (true);
         } else {
@@ -17,20 +17,66 @@ public class Stack1 {
         }
     }
 
-    public boolean isFull(int top, int size) {
-        if (top == size) {
+    public boolean isFull() {
+        if (top == size - 1) {
             return (true);
         } else {
             return (false);
         }
     }
 
-    public int top(int top) {
-        if (isEmpty(top) == true) {
-            System.out.println("Stack is Empty");
+    public int pop() {
+        if (isEmpty() == true) {
+            System.out.println("It is Empty");
+            return -1;
         } else {
-            return arr[top];
+            System.out.println("Poped Item :" + arr[top]);
+            top--;
+            return arr[top + 1];
         }
     }
 
+    public int peek() {
+        if (isEmpty() == true) {
+            System.out.println("Stack is Empty");
+            return -1;
+        } else {
+            System.out.println(arr[top]);
+            return (arr[top]);
+        }
+    }
+
+    public void push(int val) {
+        if (isFull() == true) {
+            System.out.println("Stack is Full");
+        } else {
+            top++;
+            arr[top] = val;
+            System.out.println("Number is added to the stack :" + arr[top]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack1 s = new Stack1(4);
+        s.isEmpty();
+        s.isFull();
+        s.pop();
+        s.push(10);
+        s.peek();
+        s.pop();
+        s.push(20);
+        s.push(30);
+        s.push(3);
+        s.push(34);
+        s.push(321);
+
+        s.peek();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.pop();
+    }
 }
