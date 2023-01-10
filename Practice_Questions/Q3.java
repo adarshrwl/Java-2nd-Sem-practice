b545733015e75ba916f2a33c3dbc6ebfd4f9a6ba
adarshrwl <adarshrawal321@gmail.com>
2023-01-10 22:37:03 +0545

    Question

---
+++ Practice_Questions/Q3.java
@@ -0,0 +1,17 @@
+/*Write a function that accepts an array of non-negative integers and returns the second largest integer 
+in the array. Return -1 if there is no second largest integer. 
+The signature of the function is int findSecondLargest(int[ ] a). 
+*/
+class findSecondLargest(int[ ] a){
+    
+}
+
+
+
+
+
+public class Q3 {
+    public static void main(String[] args) {
+        
+    }
+}
